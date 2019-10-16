fa99b0ee60a448629ff686d717f1bef24a733ef8
Danilo Pianini <danilo.pianini@unibo.it>
2019-10-16 19:28:20 +0200

    Enable cross-compilation

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,33 @@ gitSemVer {
     version = computeGitSemVer()
 }
 
+val java6Home: String by project //= System.getenv()["JAVA6_HOME"]
+
+    ?: throw IllegalStateException("""
+        The required environment variable JAVA6_HOME is not defined.
+        gson-extras is cross-compiled against Java 6, and requires a valid JDK6 installation to complete the build"""
+        .trimIndent()
+    )
+val java6HomeDirectory = File(java6Home).takeIf { it.exists() and it.isDirectory }
+    ?: throw IllegalStateException("JAVA6_HOME is set to $java6Home,  but it is not a valid path.")
+
+java {
+    sourceCompatibility = JavaVersion.toVersion("1.6")
+}
+
+val javaExecutablesPath = File(java6Home, "bin")
+fun javaExecutable(execName: String): String {
+    val executable = File(javaExecutablesPath, execName)
+    require(executable.exists()) { "There is no ${execName} executable in ${javaExecutablesPath}" }
+    return executable.toString()
+}
+tasks.withType<JavaCompile>().configureEach {
+    options.apply {
+        isFork = true
+        forkOptions.javaHome = file(java6Home)
+    }
+}
+
 dependencies {
     api("com.google.code.gson:gson:2.8.6")
     implementation("javax.annotation:jsr250-api:1.0")
